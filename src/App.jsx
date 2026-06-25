4db1fc8c6c077acbe5009f64e3ab50031e14fcfc
T YAGNESHWAR REDDY <tyr2203@gmail.com>
2026-06-25 15:11:02 +0530

    Add 'Skills' link to navigation

--- src/App.jsx
+++ src/App.jsx
@@ -131,6 +131,7 @@ export default function Portfolio() {
         <div className="nav-links">
           <a href="#home">Home</a>
           <a href="#about">About</a>
+          <a href="#skills">Skills</a>
           <a href="#projects">Projects</a>
           <a href="#profiles">Profiles</a>
           <a href="#contact">Contact</a>
